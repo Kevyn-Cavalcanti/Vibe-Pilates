b2a2ea9d1e21b331e9c54c88467313d4349bcca5
Lucas-Retanero <glucas.raro@gmail.com>
2025-05-08 01:09:06 -0300

    Estrutura do front pronta

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,14 @@
+const app = {
+    data() {
+        return {
+        };
+    },
+    methods: {
+    },
+    template: `
+        <h1>Vue funcionando</h1>
+    `
+};
+
+const App = Vue.createApp(app);
+App.mount('#app');
